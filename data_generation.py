9985931a2cccef8ea0495709f44cd58a5d246223
jtabalon <joseph@tabalon.io>
2023-03-11 15:22:29 -0800

    add print statements

--- data_generation.py
+++ data_generation.py
@@ -1,20 +1,21 @@
 from api import praw_id, praw_secret
-from tqdm import tqdm
 from data_helper import get_subreddit_posts
 
 import praw
 import pandas as pd
 
-reddit = praw.Reddit(client_id=praw_id, client_secret=praw_secret, user_agent='test scraper app')
+def main():
+    reddit = praw.Reddit(client_id=praw_id, client_secret=praw_secret, user_agent='test scraper app')
 
-gamestop_posts = get_subreddit_posts(reddit, 'GameStop', 100)
-tesla_posts = get_subreddit_posts(reddit, 'teslainvestorsclub', 100)
+    gamestop_posts = get_subreddit_posts(reddit, 'GameStop', 100)
+    tesla_posts = get_subreddit_posts(reddit, 'teslainvestorsclub', 100)
 
-col_names = ['title', 'score','id','subreddit','url','num_comments', 'body', 'created']
-gamestop_frame = pd.DataFrame(gamestop_posts, columns=col_names)
-tesla_frame = pd.DataFrame(tesla_posts, columns=col_names)
+    col_names = ['title', 'score','id','subreddit','url','num_comments', 'body', 'created']
+    gamestop_frame = pd.DataFrame(gamestop_posts, columns=col_names)
+    tesla_frame = pd.DataFrame(tesla_posts, columns=col_names)
 
-gamestop_frame.to_csv("gamestop.csv")
-tesla_frame.to_csv("tesla.csv")
+    gamestop_frame.to_csv("gamestop.csv")
+    tesla_frame.to_csv("tesla.csv")
 
-
+if __name__ == "__main__":
+    main()
--- data_helper.py
+++ data_helper.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 def get_subreddit_posts(reddit_client, subreddit_name, post_limit):
     posts_list = []
     subreddit_posts = reddit_client.subreddit(subreddit_name).hot(limit=post_limit)
+    print(f"Generating {post_limit} rows of {subreddit_name} data.")
     for post in tqdm(subreddit_posts, total=post_limit):
         posts_list.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
     return posts_list
