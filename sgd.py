27202b72acaa546ac1b3c173f74748182cd07f62
jtabalon <joseph@tabalon.io>
2023-03-27 16:32:58 -0700

    add imports and initialize function

---
+++ sgd.py
@@ -0,0 +1,8 @@
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+def sgd(X_train, X_test, y_train, y_test):
+    
+
+
