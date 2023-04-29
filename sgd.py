0f121d375a91aa132ca76a28fee93c33d9e9b4c9
jtabalon <joseph@tabalon.io>
2023-04-29 13:56:34 -0700

    add sgd.py

--- sgd.py
+++ sgd.py
@@ -2,7 +2,9 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
-def sgd(X_train, X_test, y_train, y_test):
-    
+def sgd(x_train, y_train):
+    sgd_clf = SGDClassifier()
+    sgd_clf.fit(x_train, y_train)
+    return sgd_clf
 
 
