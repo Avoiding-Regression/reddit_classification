542f9a28a4b4b27ced84f449ae15dd170eebae77
Brian Pak <brianpak@Brians-MacBook-Air.local>
2023-05-08 20:14:20 -0700

    adding more to model_train.py

--- model_train.py
+++ model_train.py
@@ -3,12 +3,17 @@ from sklearn.model_selection import train_test_split
 from ADAboost import adaboost_model
 from knn import k_nearest_neighbors
 from xgboost import xgboost
+from data_cleaning import data_cleaning
+from sklearn.model_selection import train_test_split
+from ADAboost import adaboost_model
+from knn import k_nearest_neighbors
+from xgboost import xgboost
 
 def main():
     data_cleaning()
     df= df
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=42)
-    adaboost_model(x_train, x_test, y_train, y_test)
+    abc(x_train, x_test, y_train, y_test)
 
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=4)
     k_nearest_neighbors(x_train, x_test, y_train, y_test)
@@ -20,6 +25,6 @@ def main():
 
 
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=4)
-    k_nearest_neighbors(x_train, x_test, y_train, y_test)
+    sgd_classifier(x_train, x_test, y_train, y_test)
 
     return adaboost_model, k_nearest_neighbors,xg_boost, sgd_classifier
