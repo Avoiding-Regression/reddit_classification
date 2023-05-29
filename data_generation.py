2a812bd9b641811ff506dc32322b6f06894998d0
brip1258 <bri.p1258@gmail.com>
2023-05-29 14:39:15 -0700

    adding data_generation.py changes

--- data_generation.py
+++ data_generation.py
@@ -3,19 +3,31 @@ from data_helper import get_subreddit_posts
 
 import praw
 import pandas as pd
+from data_cleaning import data_cleaning
 
 def main():
     reddit = praw.Reddit(client_id=praw_id, client_secret=praw_secret, user_agent='test scraper app')
 
-    gamestop_posts = get_subreddit_posts(reddit, 'GameStop', 100)
+    gamestop_posts = get_subreddit_posts(reddit, 'GameSgit chtop', 100)
     tesla_posts = get_subreddit_posts(reddit, 'teslainvestorsclub', 100)
 
     col_names = ['title', 'score','id','subreddit','url','num_comments', 'body', 'created']
     gamestop_frame = pd.DataFrame(gamestop_posts, columns=col_names)
     tesla_frame = pd.DataFrame(tesla_posts, columns=col_names)
 
-    gamestop_frame.to_csv("gamestop.csv")
-    tesla_frame.to_csv("tesla.csv")
+    gs = gamestop_frame.to_csv("gamestop.csv")
+    tesla = tesla_frame.to_csv("tesla.csv")
+
+    gs_tokens, gs = data_cleaning(gs)
+    tesla_tokens, tesla = data_cleaning(tesla)
+
+    frames = [tesla, gs]
+    final_df = pd.concat(frames)
+
+    # set labels
+
+    final_df['labels'] = np.where(final_df['subreddit'] == 'teslainvestorsclub', 1, 0)
+    final_df['text'] = final_df['text_string_lem']
 
 if __name__ == "__main__":
     main()
