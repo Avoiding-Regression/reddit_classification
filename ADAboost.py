295db13d5f396fe17f84e8ba316f6441b83c8f82
brip1258 <bri.p1258@gmail.com>
2023-06-24 13:28:03 -0700

    pretty much done

--- ADAboost.py
+++ ADAboost.py
@@ -2,14 +2,17 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 
-def adaboost_model(final_df, train_x, test_x, y_train, y_test):
+def adaboost_model(train_x, test_x, y_train, y_test):
     cv = CountVectorizer(binary = True)
     #cv.fit(final_df['text'])#don't think we need this, but we need have all the inputs be uniform 
-    train_x = cv.transform(x_train)
-    test_x = cv.transform(x_test)
+    train_x = cv.transform(train_x)
+    test_x = cv.transform(test_x)
 
     abc=AdaBoostClassifier(n_estimators = 50
                            learning_rate = 1)
+    abc.fit(train_x, test_x)
+    
+    return abc
     
 
     
--- model_inference.py
+++ model_inference.py
@@ -7,13 +7,13 @@ from data_cleaning import data_cleaning
 from sklearn.model_selection import train_test_split
 from ADAboost import adaboost_model
 from knn import k_nearest_neighbors
-from xgboost import xgboost
+from sgd import sgd
 import data_generation 
 
 def model_inf(final_df):
     df = final_df
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=42)
-    abc(x_test, y_test)
+    adaboost_model(x_test, y_test)
 
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=4)
     k_nearest_neighbors(x_test, y_test)
@@ -21,10 +21,10 @@ def model_inf(final_df):
 
     ##Change for XGBoost
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=50)
-    xg_boost(x_test, y_test)
+    xgboost(x_test, y_test)
 
 
     x_train, x_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=0.2, random_state=4)
-    sgd_classifier(x_test, y_test)
+    sgd(x_test, y_test)
 
-    return adaboost_model, k_nearest_neighbors,xg_boost, sgd_classifier
+    return adaboost_model, k_nearest_neighbors,xgboost, sgd
